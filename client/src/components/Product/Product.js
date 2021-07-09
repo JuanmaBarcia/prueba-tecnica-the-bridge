705792594447d6b76a9b7f6ac87dacabb770917b
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-07-09 21:43:43 +0200

    Revert "added basic react structure"

--- client/src/components/Product/Product.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Product.scss";
-
-function Product() {
-  return <div className='Product'>Product</div>;
-}
-
-export default Product;
--- client/src/context/appContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const appContext = React.createContext({ app: {} });
-
-export { appContext };
